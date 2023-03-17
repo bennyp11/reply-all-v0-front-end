c112ebbae6c689c50307c1283f3909d3a1a76c82
bennyp11 <ben.r.pollock@gmail.com>
2023-03-16 21:35:06 -0700

    added drawer to nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,15 +1,52 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+} from '@mui/material';
+import { Menu as MenuIcon } from '@mui/icons-material';
 
 const Nav = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
+  const menuItems = [
+    { id: 1, text: 'Home' },
+    { id: 2, text: 'About' },
+    { id: 3, text: 'Contact' },
+    // Add more menu items here
+  ];
+
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Your App Name
-        </Typography>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton color="inherit" aria-label="menu" edge="start" onClick={handleDrawerToggle}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Your App Name
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="temporary" anchor="left" open={isDrawerOpen} onClose={handleDrawerToggle}>
+        <List>
+          {menuItems.map((item) => (
+            <ListItem button key={item.id}>
+              <ListItemText primary={item.text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </>
   );
 };
 
