e5d898b075d97576b0e8c7c7d39a208923e4baad
bennyp11 <ben.r.pollock@gmail.com>
2023-03-16 21:47:48 -0700

    added theme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
+import React from 'react';
 import logo from './logo.svg';
 import { Link } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Link className="App-link" to="/start">
-          Start a Game
-        </Link>
-        <Link className="App-link" to="/join">
-          Join a Game
-        </Link>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <Link className="App-link" to="/start">
+            Start a Game
+          </Link>
+          <Link className="App-link" to="/join">
+            Join a Game
+          </Link>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
 export default App;
+
