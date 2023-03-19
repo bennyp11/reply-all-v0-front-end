81fbfbe683e9e0722b50b1d6cdd5b3e9414ba07d
bennyp11 <ben.r.pollock@gmail.com>
2023-03-18 20:33:10 -0700

    reshaping cards

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import Nav from '../components/Nav/Nav';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import './GamePage.css';
 
 function GamePage() {
   const location = useLocation();
@@ -43,6 +44,9 @@ function GamePage() {
     return <p>Loading...</p>;
   }
 
+  const topCards = dealtCards.slice(0, 4);
+  const bottomCards = dealtCards.slice(4);
+
   return (
     <>
       <Nav />
@@ -50,9 +54,10 @@ function GamePage() {
         <h1>Welcome to the game!</h1>
         <p>Nicknames: {JSON.stringify(nickNames)}</p>
         <Grid container spacing={2}>
-          {dealtCards.map((card) => (
-            <Grid item xs={12} sm={6} md={4} key={card.RelationalID}>
-              <Card>
+          <Grid className="WholeRow" container item spacing={2} xs={12}>
+            {topCards.map((card) => (
+              <Grid item xs={12} sm={6} md={3} key={card.RelationalID}>
+                <Card className="Card TopHand">
                 <CardContent>
                   <Typography variant="h5" component="h2">
                     {card.CardType}
@@ -62,8 +67,25 @@ function GamePage() {
                   </Typography>
                 </CardContent>
               </Card>
-            </Grid>
-          ))}
+              </Grid>
+            ))}
+          </Grid>
+          <Grid className="WholeRow" container item spacing={2} xs={12}>
+            {bottomCards.map((card) => (
+              <Grid item xs={12} sm={6} md={4} key={card.RelationalID}>
+                <Card className="Card BottomHand">
+                  <CardContent>
+                    <Typography variant="h5" component="h2">
+                      {card.CardType}
+                    </Typography>
+                    <Typography color="textSecondary">
+                      {card.Text}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
         </Grid>
       </div>
     </>
